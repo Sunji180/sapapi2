1c58412ec9fef80652f6ff9c7f94039b0a6212de
Sunji180 <63654200+Sunji180@users.noreply.github.com>
2025-05-20 11:39:23 +0200

    Create Enum.cs

---
+++ SAP_API2/Enum.cs
@@ -0,0 +1,87 @@
+namespace SAP_API
+{
+  public enum SearchMethods
+  {
+    DefaultValue = 0,
+    FindById = 1,
+    FindByName = 2,
+    FindByText = 3,
+    FindByObject = 4
+  }
+
+  public enum SearchMethods_Partent
+  {
+    DefaultValue = 0,
+    FindById = 1,
+    FindByName = 2,
+    FindByText = 3,
+    FindByObject = 4
+  }
+
+  public enum SapGuiComponentType
+  {
+    ___Clear___,
+    GuiApplication,
+    GuiBox,
+    GuiButton,
+    GuiCheckBox,
+    GuiCalendar,
+    GuiCollection,
+    GuiComboBox,
+    GuiComboBoxEntry,
+    GuiComponent,
+    GuiComponentCollection,
+    GuiConnection,
+    GuiContainer,
+    GuiContainerShell,
+    GuiContextMenu,
+    GuiCTextField,
+    GuiCustomControl,
+    GuiDialogShell,
+    GuiDockShell,
+    GuiFrameWindow,
+    GuiGOSShell,
+    GuiGridView,
+    GuiLabel,
+    GuiMainWindow,
+    GuiMenu,
+    GuiMenubar,
+    GuiMessageWindow,
+    GuiModalWindow,
+    GuiOkCodeField,
+    GuiPasswordField,
+    GuiRadioButton,
+    GuiScrollbar,
+    GuiScrollContainer,
+    GuiSession,
+    GuiSessionInfo,
+    GuiShell,
+    GuiShell_Subtype_GridView,
+    GuiShell_Subtype_Tree,
+    GuiSimpleContainer,
+    GuiSplit,
+    GuiSplitterContainer,
+    GuiStatusbar,
+    GuiStatusbarLink,
+    GuiStatusPane,
+    GuiTab,
+    GuiTableColumn,
+    GuiTableControl,
+    GuiTableRow,
+    GuiTabStrip,
+    GuiTextField,
+    GuiTitlebar,
+    GuiToolbar,
+    GuiToolbarControl,
+    GuiTree,
+    GuiUserArea,
+    GuiUtils,
+    GuiVComponent,
+    GuiVContainer,
+    GridView,
+    Tree,
+    Toolbar,
+    HTMLViewer,
+    TextEdit
+  }
+}
